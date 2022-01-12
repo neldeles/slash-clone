67a4881a2f0a6b5f5c1c1601fc45ccea2a2cd168
neldeles <neldeles@gmail.com>
2022-01-12 20:54:32 +0800

    style: change placeholder color of input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -13,7 +13,7 @@ export function Input({ id, name, placeholder, type }: TInputProps) {
       id={id}
       name={name}
       type={type}
-      className="peer w-full h-10 text-lg placeholder:text-base font-bold placeholder:font-normal placeholder:text-accent text-primary bg-transparent border-b-2 border-element-secondary focus:border-element-primary focus:outline-none"
+      className="peer w-full h-10 text-lg placeholder:text-base font-bold placeholder:font-normal placeholder:text-accent text-primary focus:placeholder:text-secondary bg-transparent border-b-2 border-element-secondary focus:outline-none"
       placeholder={placeholder}
     />
   );
