6508ab02544713eae92300732afa7c28ffc8e44a
neldeles <neldeles@gmail.com>
2022-01-16 18:35:35 +0800

    style: animation when mark task complete

--- src/modules/_common/components/ListItem/ListItem.tsx
+++ src/modules/_common/components/ListItem/ListItem.tsx
@@ -1,4 +1,6 @@
-import { motion } from "framer-motion";
+import { motion, useMotionValue, useTransform } from "framer-motion";
+import { useState } from "react";
+import { classNames } from "utils/classNames";
 import { Check, LeftArrow, RightArrow, Thrash } from "../Icons";
 
 type TListItemProps = {
@@ -97,6 +99,35 @@ export function ListItem({
   tasks,
   setTasks,
 }: TListItemProps) {
+  const duration = 0.4;
+
+  const markCompleteVariants = {
+    clicked: { pathLength: 1 },
+    unclicked: { pathLength: 0 },
+  };
+
+  const iconVariants = {
+    clicked: { y: [-2, 0] },
+    unclicked: { y: 0 },
+  };
+
+  const listParentVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+    exit: {
+      opacity: 0,
+      transition: {
+        when: "afterChildren",
+        duration: 0.4,
+        delay: duration - 0.2,
+        // ease: [0.04, 0.62, 0.23, 0.98],
+        ease: "anticipate",
+      },
+    },
+  };
+
+  const [doneIsClicked, setDoneIsClicked] = useState(false);
+
   const markDone = () => {
     const updatedTasks = {
       thisWeek: tasks.thisWeek.filter((task: any) => task.id !== id),
@@ -107,21 +138,56 @@ export function ListItem({
       ],
     };
 
+    setDoneIsClicked(true);
     setTasks(updatedTasks);
   };
 
   return (
     <motion.div
       className="group flex items-center"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.8, ease: [0.04, 0.62, 0.23, 0.98] }}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+      variants={listParentVariants}
     >
       {displayIndex ? <p className="mr-2 text-sm text-gray-400">{id}</p> : null}
-      <li className="flex relative flex-auto p-3 w-full text-base font-medium tracking-normal list-none text-black bg-transparent hover:bg-gray-100 active:bg-gray-200 rounded-lg border border-transparent">
+      <li className="flex relative flex-auto p-3 w-full text-base font-medium tracking-normal list-none text-black bg-transparent hover:bg-gray-100 rounded-lg border border-transparent">
         <p>{text}</p>
-        <div className="hidden group-hover:flex absolute top-1/2 right-0 justify-around items-center px-3 w-2/5 h-full group-hover:bg-gray-100 -translate-y-1/2">
+        <div
+          className={classNames(
+            "absolute top-1/2 left-0 -translate-y-1/2",
+            "flex px-3 w-full h-full items-center"
+          )}
+        >
+          <svg
+            version="1.1"
+            id="line_2"
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            width="1200px"
+            height="5px"
+          >
+            <motion.path
+              fill="#01a09e"
+              strokeWidth="6"
+              stroke="#01a09e"
+              d="M0 0 l1120 0"
+              initial={false}
+              animate={doneIsClicked ? "clicked" : "unclicked"}
+              variants={markCompleteVariants}
+              transition={{
+                duration: duration,
+              }}
+            />
+          </svg>
+        </div>
+        <div
+          className={classNames(
+            "absolute top-1/2 right-0 justify-around items-center px-3 w-2/5 h-full  -translate-y-1/2",
+            "hidden group-hover:flex group-hover:bg-gray-100"
+          )}
+        >
           {/* {buttons.map((button) => (
             <button
               key={button.name + id}
@@ -131,9 +197,30 @@ export function ListItem({
               {button.icon}
             </button>
           ))} */}
-          <ButtonIcon onClick={markDone}>
-            <Check />
-          </ButtonIcon>
+          <button
+            className=" text-gray-300 hover:text-black align-middle"
+            disabled={doneIsClicked}
+            onClick={markDone}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-6 h-6"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <motion.path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={3}
+                stroke="currentColor"
+                d="M5 13l4 4L19 7"
+                initial={false}
+                animate={doneIsClicked ? "clicked" : "unclicked"}
+                variants={iconVariants}
+                transition={{ duration: duration }}
+              />
+            </svg>
+          </button>
           <ButtonIcon onClick={markDone}>
             <LeftArrow />
           </ButtonIcon>
