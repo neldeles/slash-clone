8b0358f04f07cb0e2b3fbc07f0c0ab9d4d724c5c
neldeles <neldeles@gmail.com>
2022-01-17 14:17:37 +0800

    fix: container hover styles activate after animation
    When toggling between isActive states, the container hover style should
    only activate after the animation ends.

--- src/modules/_common/components/SideContainer/SideContainer.tsx
+++ src/modules/_common/components/SideContainer/SideContainer.tsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { useEffect, useLayoutEffect, useState } from "react";
 import { classNames } from "utils/classNames";
 
 type TProps = {
@@ -7,6 +8,28 @@ type TProps = {
   setIsActive: React.Dispatch<React.SetStateAction<boolean>>;
 };
 export function SideContainer({ isActive, setIsActive, children }: TProps) {
+  // !isActive here is just the initial value of our state
+  const [toggleHover, setToggleHover] = useState(!isActive);
+
+  /**
+   * We add these effects to delay the effectivity of the group hover classes.
+   * Without these, when the user clicks the X button, the gray hover background
+   * will flicker briefly as the cursor is hovering over the div.
+   *
+   * useLayoutEffect for when the container expands to avoid the flicker as well.
+   */
+  useLayoutEffect(() => {
+    if (isActive) {
+      setToggleHover(false);
+    }
+  }, [isActive]);
+
+  useEffect(() => {
+    if (!isActive) {
+      setTimeout(() => setToggleHover(true), 200);
+    }
+  }, [isActive]);
+
   const containerVariants = {
     active: {
       flexGrow: 1,
@@ -16,6 +39,27 @@ export function SideContainer({ isActive, setIsActive, children }: TProps) {
     },
   };
 
+  if (toggleHover) {
+    return (
+      <motion.div
+        onClick={() => {
+          if (!isActive) {
+            setIsActive(true);
+          }
+        }}
+        className={classNames(
+          "py-2 h-screen border-r border-gray-200 max-w-md",
+          "group hover:cursor-pointer hover:bg-gray-200 transition-colors duration-75"
+        )}
+        variants={containerVariants}
+        initial={false}
+        animate={isActive ? "active" : "inactive"}
+      >
+        {children}
+      </motion.div>
+    );
+  }
+
   return (
     <motion.div
       onClick={() => {
@@ -23,12 +67,7 @@ export function SideContainer({ isActive, setIsActive, children }: TProps) {
           setIsActive(true);
         }
       }}
-      className={classNames(
-        "py-2 h-screen border-r border-gray-200 max-w-md",
-        isActive
-          ? null
-          : "group hover:cursor-pointer hover:bg-gray-200 transition-colors duration-75"
-      )}
+      className={classNames("py-2 h-screen border-r border-gray-200 max-w-md")}
       variants={containerVariants}
       initial={false}
       animate={isActive ? "active" : "inactive"}
