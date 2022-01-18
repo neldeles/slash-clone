597907ab7ceb39c349ac06348ed2cf5c39cdaabb
neldeles <neldeles@gmail.com>
2022-01-18 18:42:13 +0800

    test: reset db during test setup

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,12 +3,20 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+import { db } from "mocks/db";
+import { drop } from "@mswjs/data";
 import { server } from "mocks/server";
 
 // Establish API mocking before all tests.
-beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
+beforeAll(() => {
+  server.listen({ onUnhandledRequest: "error" });
+  drop(db);
+});
 // Reset any request handlers that we may add during the tests,
 // so they don't affect other tests.
-afterEach(() => server.resetHandlers());
+afterEach(() => {
+  server.resetHandlers();
+  drop(db);
+});
 // Clean up after the tests are finished.
 afterAll(() => server.close());
