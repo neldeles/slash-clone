9cdc7afea8a4eea1b441bb1e63f5ddc8a24e16b3
neldeles <neldeles@gmail.com>
2022-01-23 15:33:41 +0800

    test: move task from Today to This Week works

---
+++ src/modules/column-today/tests/move-task.test.tsx
@@ -0,0 +1,42 @@
+import {
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+  within,
+} from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import userEvent from "@testing-library/user-event";
+import App from "App";
+import { renderWithProviders } from "utils/tests/render-with-providers";
+import { db } from "mocks/db";
+
+test("moves a task from Today column to the bottom of This Week column", async () => {
+  // We add this because scrollIntoView is not implemented in JSDOM
+  // https://stackoverflow.com/questions/53271193/typeerror-scrollintoview-is-not-a-function
+  window.HTMLElement.prototype.scrollIntoView = function () {};
+
+  const task = "move to This Week";
+  db.task.create({ task: task, status: "today", priority: 1 });
+  db.task.create({ task: "First Item", status: "thisWeek", priority: 1 });
+  db.task.create({ task: "Second Item", status: "thisWeek", priority: 2 });
+
+  renderWithProviders(<App />);
+  await waitForElementToBeRemoved(screen.queryByText(/loading/i));
+  userEvent.click(
+    await screen.findByRole("button", {
+      name: /move task in Today to This Week/i,
+    })
+  );
+  const thisWeekList = await screen.findByRole("list", { name: /this week/i });
+  const todayList = screen.getByRole("list", { name: /today/i });
+  await waitFor(() => {
+    expect(todayList).not.toHaveTextContent(task);
+  });
+  await waitFor(() => {
+    expect(thisWeekList).toHaveTextContent(task);
+  });
+  const thisWeekListItems = within(thisWeekList).getAllByRole("listitem");
+  expect(thisWeekListItems[thisWeekListItems.length - 1]).toHaveTextContent(
+    task
+  );
+});
