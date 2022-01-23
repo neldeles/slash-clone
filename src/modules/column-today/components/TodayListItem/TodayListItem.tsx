bf95b94e2c84e2fc94ba644c3ea53b57ad4fa295
neldeles <neldeles@gmail.com>
2022-01-23 14:15:51 +0800

    refactor: make TodayList use ListItem componen

--- src/modules/column-today/components/TodayListItem/TodayListItem.tsx
+++ src/modules/column-today/components/TodayListItem/TodayListItem.tsx
@@ -6,6 +6,7 @@ import {
   RightArrow,
   Thrash,
 } from "modules/_common/components/Icons";
+import { ListItem } from "modules/_common/components/ListItem";
 import { useState } from "react";
 import { classNames } from "utils/classNames";
 
@@ -21,21 +22,6 @@ export function TodayListItem({ text, id, taskIndex }: TListItemProps) {
   const markCompleteVariants = {
     clicked: { pathLength: 1 },
     unclicked: { pathLength: 0 },
-  };
-
-  const listParentVariants = {
-    hidden: { opacity: 0 },
-    visible: { opacity: 1 },
-    exit: {
-      opacity: 0,
-      transition: {
-        when: "afterChildren",
-        duration: 0.4,
-        delay: duration - 0.2,
-        // ease: [0.04, 0.62, 0.23, 0.98],
-        ease: "anticipate",
-      },
-    },
   };
 
   const [doneIsClicked, setDoneIsClicked] = useState(false);
@@ -78,65 +64,59 @@ export function TodayListItem({ text, id, taskIndex }: TListItemProps) {
   // };
 
   return (
-    <motion.div
-      className="group flex items-center"
-      initial="hidden"
-      animate="visible"
-      exit="exit"
-      variants={listParentVariants}
-    >
-      <p className="mr-2 text-sm text-gray-400">{taskIndex + 1}</p>
-      <li className="flex relative flex-auto p-3 w-full text-base font-medium tracking-normal list-none text-black bg-transparent hover:bg-gray-100 rounded-lg border border-transparent">
-        <p>{text}</p>
-        <div
-          className={classNames(
-            "absolute top-1/2 left-0 -translate-y-1/2",
-            "flex px-3 w-full h-full items-center"
-          )}
+    <ListItem>
+      <p className="absolute top-1/2 -left-5 text-sm text-gray-400 -translate-y-1/2">
+        {taskIndex + 1}
+      </p>
+      <p>{text}</p>
+      <div
+        className={classNames(
+          "absolute top-1/2 left-0 -translate-y-1/2",
+          "flex px-3 w-full h-full items-center"
+        )}
+      >
+        <svg
+          version="1.1"
+          id="line_2"
+          xmlns="http://www.w3.org/2000/svg"
+          x="0px"
+          y="0px"
+          width="1200px"
+          height="5px"
         >
-          <svg
-            version="1.1"
-            id="line_2"
-            xmlns="http://www.w3.org/2000/svg"
-            x="0px"
-            y="0px"
-            width="1200px"
-            height="5px"
-          >
-            <motion.path
-              fill="#01a09e"
-              strokeWidth="6"
-              stroke="#01a09e"
-              d="M0 0 l1120 0"
-              initial={false}
-              animate={doneIsClicked ? "clicked" : "unclicked"}
-              variants={markCompleteVariants}
-              transition={{
-                duration: duration,
-              }}
-            />
-          </svg>
-        </div>
-        <div
-          className={classNames(
-            "absolute top-1/2 right-0 justify-end items-center gap-2 px-3 w-2/5 h-full  -translate-y-1/2",
-            "hidden group-hover:flex group-hover:bg-gray-100"
-          )}
-        >
-          <IconButton>
-            <Check />
-          </IconButton>
-          <IconButton>
-            <LeftArrow />
-          </IconButton>
-          <IconButton>
-            <RightArrow />
-          </IconButton>
-          <IconButton>
-            <Thrash />
-          </IconButton>
-        </div>
-      </li>
-    </motion.div>
+          <motion.path
+            fill="#01a09e"
+            strokeWidth="6"
+            stroke="#01a09e"
+            d="M0 0 l1120 0"
+            initial={false}
+            animate={doneIsClicked ? "clicked" : "unclicked"}
+            variants={markCompleteVariants}
+            transition={{
+              duration: duration,
+            }}
+          />
+        </svg>
+      </div>
+      <div
+        className={classNames(
+          "absolute top-1/2 right-0 justify-end items-center gap-2 px-3 w-2/5 h-full  -translate-y-1/2",
+          "hidden group-hover:flex group-hover:bg-gray-100"
+        )}
+      >
+        <IconButton>
+          <Check />
+        </IconButton>
+        <IconButton>
+          <LeftArrow />
+        </IconButton>
+        <IconButton>
+          <RightArrow />
+        </IconButton>
+        <IconButton>
+          <Thrash />
+        </IconButton>
+      </div>
+    </ListItem>
   );
 }
