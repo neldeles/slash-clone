73b8acc15455359b3505bede5fc70e567718890c
neldeles <neldeles@gmail.com>
2022-01-27 11:02:28 +0800

    test: rewrite test to avoid random fails
    Test randomly fails with reason that task is still on screen. Rewrite
    by waiting for text to be removed from source list.

--- src/modules/Main/components/ThisWeek/tests/mark-task-done.test.tsx
+++ src/modules/Main/components/ThisWeek/tests/mark-task-done.test.tsx
@@ -2,6 +2,7 @@ import {
   screen,
   waitFor,
   waitForElementToBeRemoved,
+  within,
 } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import userEvent from "@testing-library/user-event";
@@ -20,11 +21,12 @@ test("moves a task from This Week column to the Done column", async () => {
   renderWithProviders(<App />);
   await waitForElementToBeRemoved(screen.queryByText(/loading/i));
   userEvent.click(await screen.findByRole("button", { name: /mark-done/i }));
-  const thisWeekList = await screen.findByRole("list", { name: /this week/i });
+
+  const thisWeekList = screen.getByRole("list", { name: /this week/i });
   const doneList = screen.getByRole("list", { name: /done/i });
-  await waitFor(() => {
-    expect(thisWeekList).not.toHaveTextContent(task);
-  });
+
+  await waitForElementToBeRemoved(within(thisWeekList).queryByText(task));
+  expect(thisWeekList).not.toHaveTextContent(task);
   await waitFor(() => {
     expect(doneList).toHaveTextContent(task);
   });
